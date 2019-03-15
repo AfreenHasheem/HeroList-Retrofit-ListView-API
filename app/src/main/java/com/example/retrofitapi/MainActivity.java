38720f7034f4ca292f0a6e2ced88dafb90a40715
Siraj <asrf@C02SG255G8WN.local>
2019-03-15 16:11:14 +0000

    A list of names of Movies using Retrofit

---
+++ app/src/main/java/com/example/retrofitapi/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.retrofitapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewHeroes);
+
+        getHeroes();
+
+    }
+
+    private void getHeroes(){
+        //create retrofit object
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        //call the API
+        Call<List<HeroModel>> call = api.getHeroes();
+
+        call.enqueue(new Callback<List<HeroModel>>() {
+            @Override
+            public void onResponse(Call<List<HeroModel>> call, Response<List<HeroModel>> response) {
+                //get the list of heroes
+
+                List<HeroModel> heroesList = response.body();
+
+               String[] heroes = new String[heroesList.size()];
+
+                for (int i = 0; i < heroesList.size(); i++) {
+                    heroes[i] = heroesList.get(i).getName();
+                }
+
+                //displaying the string array into listview
+                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, heroes));
+
+
+//                for(HeroModel h: heroesList){
+//                    Log.d("name", h.getName());
+//                    Log.d("real name", h.getRealname());
+//                    Log.d("team", h.getTeam());
+//                    Log.d("firstappearance", h.getFirstappearance());
+//                    Log.d("createdby", h.getCreatedby());
+//                    Log.d("publisher", h.getPublisher());
+//                    Log.d("imageurl", h.getImageurl());
+//                    Log.d("bio", h.getBio());
+//
+//                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<HeroModel>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
+
